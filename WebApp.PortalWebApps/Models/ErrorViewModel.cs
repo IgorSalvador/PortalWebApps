bbed04187daa67c0a3c2009ac4e81c326ee2aab0
Igor Henrique Salvador <igorsalvador0621@gmail.com>
2024-12-22 11:22:47 -0300

    Creating Project

---
+++ WebApp.PortalWebApps/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PortalWebApps.WebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
